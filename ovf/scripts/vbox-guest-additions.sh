4347af00ab32203463d45711b636c95ae3cc249c
Jack S. Hale <mail@jackhale.co.uk>
2015-08-24 18:26:14 +0200

    Revert "Don't see the need for this script with the ubuntu package in place."
    This reverts commit 26ecc72159a160b272891d40c367506d46f28eeb.

---
+++ ovf/scripts/vbox-guest-additions.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+mkdir /tmp/virtualbox
+VERSION=$(cat /home/fenics/.vbox_version)
+mount -o loop /home/vagrant/VBoxGuestAdditions_$VERSION.iso /tmp/virtualbox
+sh /tmp/virtualbox/VBoxLinuxAdditions.run
+umount /tmp/virtualbox
+rmdir /tmp/virtualbox
+rm /home/fenics/*.iso
